012e9170272c2afb27ca2660ac49d1875a0c1c11
Jean Pool <poolgamer0920u@gmail.com>
2023-10-03 17:45:10 -0400

    terminado navbar falta personalizar

--- EMBATETION/resources/views/Navbar/Navbar.blade.php
+++ EMBATETION/resources/views/Navbar/Navbar.blade.php
@@ -2,45 +2,47 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>EMBATE</title>
+    <script src="https://kit.fontawesome.com/13fe1b16d3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Version inicial</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Link
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <header>
+        <div class="container__menu">
+            <div class="menu">
+              <input type="checkbox" id="check__menu">
+              <label  for="check__menu" id="label__check">
+                <i class="fa-solid fa-bars icon__menu"></i>
+              </label>
+              <nav>
+                <ul>
+                  <li><a href="#">Logo</a></li>
+                  <li><a href="#">Acerca de nosotros</a>
+                    <ul>
+                      <li><a href="">Quienes somos</a></li>
+                      <li><a href="">Nuestro equipo</a></li>
+                      <li><a href="">Como trabajamos</a></li>
+                      <li><a href="">Mision, Vision y valores</a></li>
+                      <li><a href="">Premios y reconocimientos</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Servicios</a></li>
+                  <li><a href="#">Tienda</a></li>
+                  <li><a href="#">Aprender</a></li>
+                  <li><a href="#">Blog</a></li>
                 </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Link</a>
-              </li>
-            </ul>
-            
-          </div>
+              </nav>
+
+            </div>
         </div>
-      </nav>
+
+    </header>
+
+
+   <script type="text/javascript" src="{{asset('js/navbar.js')}}"></script>
+
       <main class="py-4">
          @yield('content')
       </main>
