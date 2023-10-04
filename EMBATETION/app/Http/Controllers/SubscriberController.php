0729702732310f2b49195125838885238fa32608
mariela-real <fernandezrealmariela@gmail.com>
2023-10-04 17:12:53 -0400

    migrations

---
+++ EMBATETION/app/Http/Controllers/SubscriberController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subscriber;
+use Illuminate\Http\Request;
+
+class SubscriberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function viewRegistration()
+    {
+        $subscribers = Carrera::all();
+        return view('subscriber.subscriber-registration');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function registro(Request $request)
+    {
+        $subscribers = new Materia();
+        $subscribers->firstName = $request->firstName;
+        $subscribers->lastName = $request->lastName;
+        $subscribers->email = $request->email;
+        $subscribers->save();
+       
+        return redirect()->route('subscribers')->with('registrar', 'ok');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subscriber $subscriber)
+    {
+        //
+    }
+}
